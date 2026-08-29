de192e2e8711a806f8248e077efee5b45c9a74e7
Vladimir <vladimir.lima.1950@gmail.com>
2026-08-29 13:37:47 -0300

    Correções no Mailer

--- pipeline/mailer.php
+++ pipeline/mailer.php
@@ -1,4 +1,3 @@
-<?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -15,29 +14,43 @@ function enviarEmailTitan($para, $assunto, $mensagemHTML) {
         $mail->Host       = 'smtp.titan.email';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'vladimir.lima@mupeconsult.com';
-        $mail->Password   = 'Vlagoshost1950#';
+        $mail->Password   = 'Vlagoshost1950#'; // deixamos assim por enquanto
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
-		$mail->CharSet = 'UTF-8';
-		$mail->Encoding = 'base64';
+        $mail->CharSet = 'UTF-8';
+        $mail->Encoding = 'base64';
 
+        // Necessário para Railway
+        $mail->SMTPOptions = [
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            ]
+        ];
 
         $mail->setFrom('vladimir.lima@mupeconsult.com', 'MUPE Consultoria');
         $mail->addAddress($para);
+
+        // Anexos das planilhas
+        $mail->addAttachment('/app/clientes/7/saida/tabela_apres2.xlsx');
+        $mail->addAttachment('/app/clientes/7/saida/tabela_demandas_previsoes.xlsx');
+        $mail->addAttachment('/app/clientes/7/saida/tabela_estoques_segurança.xlsx');
+        $mail->addAttachment('/app/clientes/7/saida/tabela_estoques_valores.xlsx');
+        $mail->addAttachment('/app/clientes/7/saida/tabela_tempo_programas.xlsx');
 
         $mail->isHTML(true);
         $mail->Subject = $assunto;
         $mail->Body    = $mensagemHTML;
 
         if (!$mail->send()) {
-            echo "<b>Erro ao enviar email:</b> " . $mail->ErrorInfo;
+            error_log("Erro ao enviar email: " . $mail->ErrorInfo);
         } else {
-            echo "<b>Email enviado com sucesso.</b>";
+            error_log("Email enviado com sucesso para: $para");
         }
 
     } catch (Exception $e) {
-        echo "<b>Erro PHPMailer:</b> " . $mail->ErrorInfo;
+        error_log("Erro PHPMailer: " . $e->getMessage());
     }
 }
-?>
