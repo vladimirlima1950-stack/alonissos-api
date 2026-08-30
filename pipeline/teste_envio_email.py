af8a1c3ce7844c8b02815d78095ea8e2b9c6dc1c
Vladimir <vladimir.lima.1950@gmail.com>
2026-08-30 15:29:07 -0300

    Adicionado programa de teste de envio de e-mail via Mailtrap

---
+++ pipeline/teste_envio_email.py
@@ -0,0 +1,20 @@
+@app.get("/teste-email")
+def teste_email():
+    import smtplib
+    from email.mime.text import MIMEText
+
+    msg = MIMEText("Teste de envio de e-mail via Railway.")
+    msg["Subject"] = "Teste Railway"
+    msg["From"] = "vladimir.lima.1950@gmail.com"
+    msg["To"] = "vladimir.lima.mupe.consultoria@gmail.com"
+
+    try:
+        with smtplib.SMTP("smtp.seuservidor.com", 587, timeout=20) as server:
+            server.starttls()
+            server.login("vladimir.lima.mupe.consultoria", "db6531f2c852b38da49885d3bb491bff")
+            server.send_message(msg)
+
+        return {"status": "OK", "mensagem": "E-mail enviado com sucesso!"}
+
+    except Exception as e:
+        return {"status": "ERRO", "mensagem": str(e)}
